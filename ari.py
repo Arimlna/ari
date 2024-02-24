df48316e729e1d66cf75169d3cbe3c863335b850
Arimlna <160694744+Arimlna@users.noreply.github.com>
2024-02-24 13:17:29 +0700

    Add files via upload

---
+++ ari.py
@@ -0,0 +1,14 @@
+# variable adalah tempat menyimpan data#
+
+#menaru / assigment#
+a = 10
+x = 5
+panjang = 1000
+
+print ("Nilai a = ", a)
+print ("Nilai x = ", x)
+print ("Nilai panjang = ", panjang)
+
+#penamaan#
+nilai_y = 15
+print ("nilai y = ", x)
